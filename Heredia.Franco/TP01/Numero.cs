b89bc3353d7becaa6e7eb195df5fe3035bccf331
FrancoFelicianoClarosHeredia <f.f.c.h_1991@hotmail.com>
2016-09-09 13:03:09 -0300

    02
    02

---
+++ Heredia.Franco/TP01/Numero.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP01
+{
+    class Numero
+    {
+        private double numero;
+
+        public Numero()
+        {
+            this.numero = 0;
+        }
+
+        public Numero(double num)
+        {
+            this.numero = num;
+        }
+
+        public Numero(string num)
+        {
+            
+        }
+
+        public double validarNumero(string numeroString)
+        {
+            double retorno=0;
+
+            if (double.TryParse(numeroString, out retorno))
+            {
+                return retorno;
+            }
+
+            return retorno;
+        }
+
+        public double getNumero
+        {
+            get { return this.numero; }
+        }
+
+        private void setNumero(string numero)
+        {
+            set { this.numero=validarNumero(numero); }
+        }
+
+
+    }
+}
